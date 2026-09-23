f02bc7e40d07e07818371df111c43e014d6e1aee
Janhavi Patil <jannp2007@gmail.com>
2026-09-23 19:26:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Hiee wrud?? ")
+
+var a = 3;
+var b = 4;
+a = a + 1;
+var c = "Juliett";
+
+//var 55a = "Romeo";......Not allowed
+
+console.log(a + b + 9);
+console.log(typeof a , typeof b, typeof c);
+
+//const a1 = 4;
+//a1 =  a1 + 4;..... Not allowed because it is constant .
+  //let a = 2;
+ // let b = 5;
+// {
+   // let a = 55;
+  //  console.log(a)
+ //}
+// console.log(a)
+
+
+
+// datatypes
+
+let x = "romeo and juliett";
+let y = 20;
+let z = 8.25;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+//object
+
+let i = {
+    name : "Romeo",
+    "College name" :"RCPIT"
+}   
+console.log(i)
+i.salary = "500crores";
+console.log(i)
