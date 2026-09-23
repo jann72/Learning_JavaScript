2be57e33033eb605c06ba3663caeb3c32f9fc976
Janhavi Patil <jannp2007@gmail.com>
2026-09-23 21:54:29 +0530

    operators

---
+++ Conditionals Statement/script.js
@@ -0,0 +1,43 @@
+console.log("Hiee I am here to learn conditional Statements");
+//Arithmetic Operators
+let age = 0;
+let grace = 2;
+
+// console.log (age + grace)
+// console.log (age - grace)
+// console.log (age * grace)
+// console.log (age / grace)
+// console.log (age ** grace)
+// console.log (age % grace)
+
+// if ((age+grace)>=20){
+//     console.log("You can drink alcohol");
+// }
+// else {
+//     console.log("You cannot drink alcohol");
+// }
+
+// assignment operators
+//comparison operators
+// logical operators
+
+
+
+// conditionl statement
+//1) if 
+//2) if else
+//3)if..else if...else multiple
+if (age>=23){
+    console.log("You can drink alcohol");
+}
+else if(age == 3){
+    console.log("Are you thodasaa mad??")
+}
+else {
+    console.log("You cannot drink alcohol");
+}
+
+// ternary operator
+ var a = 3;
+var b = 4;
+let c = a > b ? ( a - b) : (b - a);
